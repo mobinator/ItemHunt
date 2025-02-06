182ffe0ec3fb21c82f6b4837ccc1a0dbd57b4974
mobinator <61843499+mobinator@users.noreply.github.com>
2025-02-06 14:05:59 +0100

    feat: skip Item only possible in Singe Item Mode

--- src/main/kotlin/org/nico/itemHunt/command/commands/SkipItem.kt
+++ src/main/kotlin/org/nico/itemHunt/command/commands/SkipItem.kt
@@ -4,6 +4,8 @@ import io.papermc.paper.command.brigadier.BasicCommand
 import io.papermc.paper.command.brigadier.CommandSourceStack
 import net.kyori.adventure.text.Component
 import org.bukkit.entity.Player
+import org.jetbrains.annotations.ApiStatus
+import org.nico.itemHunt.game.data.GameData
 import org.nico.itemHunt.teams.ItemHuntTeam
 
 class SkipItem : BasicCommand {
@@ -13,6 +15,10 @@ class SkipItem : BasicCommand {
 
         if (commandSourceStack.sender !is Player) {
             commandSourceStack.sender.sendMessage(Component.text("You must be a player to use this command"))
+            return
+        }
+        if (GameData.listMode) {
+            commandSourceStack.sender.sendMessage(Component.text("You can't skip items in list mode"))
             return
         }
         val player = commandSourceStack.sender as Player
